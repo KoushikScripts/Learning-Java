48b7ddde5fd1cd82e2fe264ce3fe28874dba325b
KoushikScripts <kauschik@yahoo.com>
2024-12-20 22:05:27 +0530

    gui practice

--- gui.java
+++ gui.java
@@ -1,9 +1,12 @@
+import javax.print.attribute.standard.JobHoldUntil;
 import javax.swing.JOptionPane;
 
 public class gui {
     public static void main (String[] agrs){
         String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "hello "+name);
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age "));
+        JOptionPane.showMessageDialog(null, "You are "+age+ "Years old");
          
     }
 }
