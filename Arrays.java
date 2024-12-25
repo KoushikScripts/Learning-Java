7605b7c96d0778e0b00bdc3b142c1dab290defc1
KoushikScripts <kauschik@yahoo.com>
2024-12-25 21:43:31 +0530

    arrays

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main (String[] args) {
+
+
+        String[] cars = {"tata","toyota", "VW" };
+        
+        cars[0]="Ford";
+
+        System.out.println(cars[2]);
+    }
+}
