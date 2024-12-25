0c9715fb3def8373288499d3e22083b2eb504a94
KoushikScripts <kauschik@yahoo.com>
2024-12-25 21:52:02 +0530

    arrays

--- Arrays.java
+++ Arrays.java
@@ -2,23 +2,29 @@ public class Arrays {
     public static void main (String[] args) {
 
 
-        // String[] cars = {"tata","toyota", "VW" };
+        String[] cars = {"tata","toyota", "VW" };
         
-        // cars[0]="Ford";
+        cars[0]="Ford";
 
         // System.out.println(cars[2]);
 
-        //another way of wrting or assigning arrays
+        // another way of wrting or assigning arrays
 
-        String[] drinks = new String[4];
+        String[] drinks = new String[4]; // first declare the elements that we would like to use
 
+        // now assigne/declare elements
         drinks[0] = "Coke";
         drinks[1] = "Pepsi";
         drinks[2] = "coffee";
         drinks[3] = "orange juice";
         //drinks[4] = "apple juice";
 
-        System.out.println(drinks[3]);
+       // System.out.println(drinks[3]);
 
+
+        // using for loops
+        for (int i = 0; i<drinks.length; i++){
+            System.out.println(drinks[i]);
+        }
     }
 }
