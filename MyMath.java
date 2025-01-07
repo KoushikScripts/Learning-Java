0a9bead9e29d9a124fe832c51b00e3febc68c109
KoushikScripts <kauschik@yahoo.com>
2025-01-07 21:09:38 +0530

    2DArrays

--- MyMath.java
+++ MyMath.java
@@ -3,6 +3,6 @@ public class MyMath {
     public static void main(String[] args) {
         
         System.out.println(Math.PI);
-        System.out.println("This is the value of pi: ");
+        System.out.println("This is the value of pi:   ");
     }
 }
