3a8486ab832caa487238819f3ca5a7c8d0cac5ed
KoushikScripts <kauschik@yahoo.com>
2025-01-07 21:08:16 +0530

    2DArrays

---
+++ Maths.java
@@ -0,0 +1,7 @@
+public class Maths {
+    public static void main(String[] args) {
+        
+        System.out.println(Math.PI);
+    }
+    
+}
---
+++ MyMath.java
@@ -0,0 +1,8 @@
+public class MyMath {
+
+    public static void main(String[] args) {
+        
+        System.out.println(Math.PI);
+        System.out.println("This is the value of pi: ");
+    }
+}
