0aa42ed3c70968531371b9a07af9cf1138e9d2d6
KoushikScripts <kauschik@yahoo.com>
2025-01-13 17:14:26 +0530

    Auto-update: 2025-01-13 17:14:26

---
+++ EODPush.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ensure the script stops on error
+set -e
+
+# change directory to Obsidian vault
+cd /home/reddy/codes/Learning-Java
+
+# check for any changes
+if git diff --quiet && git diff --cached --quiet; then
+    echo "No changes to commit. Exiting."
+    exit 0
+fi
+
+# stage all changes
+git add .
+
+# commit with a timestamp
+commit_message="Auto-update: $(date +"%Y-%m-%d %H:%M:%S")"
+git commit -m "$commit_message"
+
+# push changes
+git push
--- variables.java
+++ variables.java
@@ -2,7 +2,8 @@ public class variables {
     public static void main (String[] args) {
 
         double age = 21.5;
-        System.out.println(age);
+        double Mage = 33.5;
+        System.out.println(Mage);
     }
     
 }
