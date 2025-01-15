7bb169fdd94e75bf7336394c8d794340f13b6a1e
KoushikScripts <kauschik@yahoo.com>
2025-01-15 21:45:12 +0530

    Java Learning Progress: 2025-01-15 21:45:12

--- IfStatements.java
+++ IfStatements.java
@@ -1,6 +1,13 @@
 public class IfStatements {
     public static void main(String[] args) {
-        
+        int age = 15;
+
+        if (age>=18) {
+            System.out.println("you are and adult!");
+        }
+        else {
+            System.out.println("You are still a child!");
+        }
     }
     
 }
